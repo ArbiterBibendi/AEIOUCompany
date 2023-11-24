2156fb8b81acc7d8754f91366f796a8ff4254160
ArbiterBibendi <ArbiterBibendi@yahoo.com>
2023-11-24 17:50:51 -0600

    moving in

---
+++ SpeakServer/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+using SharpTalk;
+
+namespace Speak
+{
+    class Program
+    {
+        static bool _shouldCloseConnection = false;
+        static bool _shouldCloseServer = false;
+
+        static NamedPipeServerStream namedPipeServerStream = null;
+        static StreamReader streamReader = null;
+        static StreamWriter streamWriter = null;
+        static FonixTalkEngine tts = null;
+
+        static readonly string MESSAGE_PREFIX = "msg=";
+        static readonly string MESSAGE_ALOUD_PREFIX = "msgA=";
+        static void Main()
+        {
+            namedPipeServerStream = new NamedPipeServerStream("AEIOUCOMPANYMOD");
+            streamReader = new StreamReader(namedPipeServerStream, Encoding.UTF8, true, 8192, true);
+            streamWriter = new StreamWriter(namedPipeServerStream, Encoding.UTF8, 8192, true);
+            tts = new FonixTalkEngine();
+
+            while (!_shouldCloseServer)
+            {
+                namedPipeServerStream.WaitForConnection();
+                Console.WriteLine("Connected");
+
+                ListenForMessages();
+
+                namedPipeServerStream.Disconnect();
+            }
+            tts.Sync();          
+        }
+        static void ListenForMessages()
+        {
+            while (!_shouldCloseConnection)
+            {
+                try
+                {
+                    string line = streamReader.ReadLine();
+                    Console.WriteLine(line);
+                    HandleMessage(line);
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine($"Broken: {e.Message}");
+                    namedPipeServerStream.Disconnect();
+                    continue;
+                }
+            }
+        }
+        static void HandleMessage(string line)
+        {
+            if (line == null || line == "")
+            {
+                return;
+            }
+            else if (line == "exit")
+            {
+                _shouldCloseConnection = true;
+                return;
+            }
+            else if (line.StartsWith(MESSAGE_PREFIX, StringComparison.Ordinal))
+            {
+                Console.WriteLine("Stream");
+                string message = line.Substring(MESSAGE_PREFIX.Length);
+                tts.SpeakToStream(namedPipeServerStream, message);
+            }
+            else if (line.StartsWith(MESSAGE_ALOUD_PREFIX, StringComparison.Ordinal))
+            {
+                Console.WriteLine("Speaker");
+                string message = line.Substring(MESSAGE_ALOUD_PREFIX.Length);
+                tts.Speak(message);
+            }
+        }
+    }
+}
