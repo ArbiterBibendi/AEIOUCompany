5ff357479baa546c3c0b1a25a513986060ad114a
ArbiterBibendi <ArbiterBibendi@yahoo.com>
2023-11-29 19:52:46 -0600

    build scripts make more sense now

--- Plugin/build.bat
+++ Plugin/build.bat
@@ -2,10 +2,5 @@
 set "OUTPUT_DIRECTORY=..\release\aeioucompany\"
 
 dotnet build
-
 copy "bin\Debug\netframework4.8\AEIOUCompany.dll" "%OUTPUT_DIRECTORY%"
-copy "lib\SharpTalk.dll" "%OUTPUT_DIRECTORY%"
-copy "lib\FonixTalk.dll" "%OUTPUT_DIRECTORY%"
-copy "lib\ftalk_us.dic" "%OUTPUT_DIRECTORY%"
-copy "lib\ftalk_us.dll" "%OUTPUT_DIRECTORY%"
 exit
--- SpeakServer/build.bat
+++ SpeakServer/build.bat
@@ -3,4 +3,8 @@ set "OUTPUT_DIRECTORY=..\release\aeioucompany"
 mkdir "%OUTPUT_DIRECTORY%""
 dotnet build
 copy "bin\Debug\AEIOUSpeak.exe" "%OUTPUT_DIRECTORY%\AEIOUSpeak.exe"
+copy "SharpTalk.dll" "%OUTPUT_DIRECTORY%"
+copy "FonixTalk.dll" "%OUTPUT_DIRECTORY%"
+copy "ftalk_us.dic" "%OUTPUT_DIRECTORY%"
+copy "ftalk_us.dll" "%OUTPUT_DIRECTORY%"
 exit
